87518efd8ea58fa3b94e140d5b5fe0e15ab58d54
sircoolio <ericklein@users.noreply.github.com>
2020-10-07 15:28:05 -0700

    Adafruit IO MQTT
    Added Adafruit IO MQTT sub support, replacing Adafruit IO API access. Also added untested MQTT broker code.

--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 # Status light
 
 ## Purpose 
-Traffic light triggered by changes in cloud-based data source
+Status light triggered by changes in MQTT feed
 
 ### Contributors
 
@@ -18,23 +18,33 @@ Traffic light triggered by changes in cloud-based data source
 - Adafruit Huzzah pinout - https://learn.adafruit.com/adafruit-huzzah-esp8266-breakout/pinouts
 - Relay setup - http://www.circuitbasics.com/setting-up-a-5v-relay-on-the-arduino/
 - ESP8266 Arduino from: https://github.com/esp8266/Arduino
+- MQTT
+	- https://learn.adafruit.com/adafruit-io/mqtt-api
+	- https://learn.adafruit.com/mqtt-adafruit-io-and-you
 
 ### Learnings
 
 ### Issues
 
 ### Feature Requests
-- 111118: Handle error condition where connection to Adafruit IO does not happen
-- 111118: Add initial read to door state, it currently waits for a subsequent message
-- 120219: Fritzing diagram
+- 111118: [P2]; Handle error condition where connection to Adafruit IO does not happen
+- 100120: [P1]; support MQTT brokers
+- 100720: [P3]; try and integrate two MQTT code paths?
+- 100720: [P2]; implement MQTT QoS and Will
 
 ### Revisions
 - 102118: First version, branched from Adafruit IO read example
-- 102618: opsec issues fixed
+- 102618: infosec issue fixed
 - 111118
-	- [fr]10/21/18: move IO feed name from code to config.h
+	- [FR]102118: move IO feed name from code to config.h
 	- relay trigger code added
 - 092920
 	- updated to secrets.h for credential data only
 	- conditional compile for networks, debug, and sleep
 	- added Ethernet support (not tested though ported from working code)
+	- [FR]111118, Add initial read to door state, it currently waits for a subsequent message -> added get() in setup() to fix this
+- 100720
+	- removed SLEEP support
+	- removed Adafruit IO API support
+	- [FR]100120: [P1]; Support Adafruit IO MQTT
+	- untested code for MQTT brokers
--- status_light.ino
+++ status_light.ino
@@ -1,7 +1,7 @@
 /*
   Project Name:   status light
   Developer:      Eric Klein Jr. (temp2@ericklein.com)
-  Description:    Traffic light triggered by changes in cloud-based data source
+  Description:    Status light triggered by changes in cloud-based data source
   
   See readme.md for target information, revision history, feature requests, etc.
 */
@@ -10,47 +10,52 @@
 #define DEBUG     // Output to the serial port
 //#define RJ45    // use Ethernet to send data to cloud services
 #define WIFI      // use WiFi to send data to cloud services
-//#define SLEEP   // sleeps ESP8266 for extended periods between cloud service updates
-
-// Adafruit IO and network device setup
-#include "secrets.h"
-
-#ifdef WIFI
-  #include "AdafruitIO_WiFi.h"
-
-  #if defined(USE_AIRLIFT) || defined(ADAFRUIT_METRO_M4_AIRLIFT_LITE) ||         \
-    defined(ADAFRUIT_PYPORTAL)
-    // Configure the pins used for the ESP32 connection
-    #if !defined(SPIWIFI_SS) // if the wifi definition isnt in the board variant
-    // Don't change the names of these #define's! they match the variant ones
-      #define SPIWIFI SPI
-      #define SPIWIFI_SS 10 // Chip select pin
-      #define NINA_ACK 9    // a.k.a BUSY or READY pin
-      #define NINA_RESETN 6 // Reset pin
-      #define NINA_GPIO0 -1 // Not connected
-    #endif
-    AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS, SPIWIFI_SS,
-                   NINA_ACK, NINA_RESETN, NINA_GPIO0, &SPIWIFI);
-  #else
-    AdafruitIO_WiFi io(AIO_USERNAME, AIO_KEY, WIFI_SSID, WIFI_PASS);  // defined in secrets.h
-  #endif
-#endif
-
-#ifdef RJ45
-  #include "AdafruitIO_Ethernet.h"
-  AdafruitIO_Ethernet io(AIO_USERNAME, AIO_KEY); // defined in secrets.h
-#endif
-
-// Adafruit IO feeds
-AdafruitIO_Feed *statusLight = io.feed("status-light");
 
 // relay featherwing support
 #define relayTriggerPIN 12
 
-#ifdef SLEEP
-  // deep sleep length before checking door state in seconds
-  #define SLEEP_LENGTH 18e7
+// Adafruit IO and network device setup
+#include "secrets.h"
+
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+// GENERIC #include <ArduinoMqttClient.h>
+
+#ifdef WIFI
+  #if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_AVR_UNO_WIFI_REV2)
+    #include <WiFiNINA.h>
+  #elif defined(ARDUINO_SAMD_MKR1000)
+    #include <WiFi101.h>
+  #elif defined(ARDUINO_ESP8266_ESP12)
+    #include <ESP8266WiFi.h>
+  #endif
+
+  WiFiClient client;
+
+  //use WiFiClientSecure for SSL
+  //WiFiClientSecure client;
 #endif
+
+#ifdef RJ45
+  // Set MAC address. If unknown, be careful for duplicate addresses across projects.
+  byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+  #include <SPI.h>
+  #include <Ethernet.h>
+  //#include <EthernetClient.h>  // Adafruit says required, not so in my experience?
+  EthernetClient client;
+#endif
+
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, MQTT_PORT, AIO_USERNAME, AIO_KEY);
+
+// GENERIC
+// #include <ArduinoMqttClient.h>
+// MqttClient mqttClient(client);
+
+// GENERIC MQTT client and topic names
+// char statusLightTopic[] = "status-light";
+
+Adafruit_MQTT_Subscribe statusLight = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/status-light");
 
 void setup() 
 {
@@ -65,57 +70,131 @@ void setup()
 
   pinMode(relayTriggerPIN, OUTPUT);
 
-  // message handler for the status-light feed ,called whenever a message is received from adafruit io
-  statusLight->onMessage(handleMessage);
+  // Connect to WiFi access point.
+  #ifdef DEBUG
+    Serial.print("Connecting to ");
+    Serial.println(WIFI_SSID);
+  #endif
 
-  io.connect();
-  // wait for a connection
-  while (io.status() < AIO_CONNECTED)
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) 
   {
     #ifdef DEBUG
-      Serial.println(io.statusText());
       Serial.print(".");
     #endif
     delay(500);
   }
-  // we are connected
+
   #ifdef DEBUG
-    Serial.println();
-    Serial.println(io.statusText());
+    Serial.println();  // finishes the status dots print
+    Serial.println("WiFi connected, IP address: ");
+    Serial.println(WiFi.localIP());
   #endif
 
-  statusLight->get();
+  // try to connect to the MQTT broker once you're connected to WiFi:
+  // while (!connectToBroker())
+  // {
+  //   Serial.println("attempting to connect to broker");
+  //   delay(1000);
+  // }
+  // Serial.println("connected to broker");
+
+  // set the credentials for the MQTT client:
+  // GENERIC mqtt.setId(clientID);
+  // GENERIC mqttClient.setUsernamePassword(SECRET_MQTT_USER, SECRET_MQTT_PASS);
+  mqtt.subscribe(&statusLight);
 }
 
 void loop()
 {
-  // Keep connection to Adafruit IO open
-  io.run();
-}
+  // Keep connection to MQTT broker open
+  // GENERIC
+  // mqttClient.poll();
 
-// handles Adafruit IO feed message instantiated in setup()
-void handleMessage(AdafruitIO_Data *data) 
-{ 
-  if(data->toPinLevel() == LOW)
-  { 
-    #ifdef DEBUG
-      Serial.println("turning light off");
-    #endif
-    digitalWrite(relayTriggerPIN, LOW);
-  }
-  else
+  //   if (!mqttClient.connected()) {
+  //   Serial.println("reconnecting");
+  //   connectToBroker();
+  // }
+
+  // // if a message comes in, read it:
+  // if (mqttClient.parseMessage() > 0) {
+  //   Serial.print("Got a message on topic: ");
+  //   Serial.println(mqttClient.messageTopic());
+  //   // read the message:
+  //   while (mqttClient.available()) {
+  //     // convert numeric string to an int:
+  //     int message = mqttClient.parseInt();
+  //     Serial.println(message);
+
+  MQTT_connect();
+
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(5000))) 
   {
-    #ifdef DEBUG
-      Serial.println("turning light on");
-    #endif
-    digitalWrite(relayTriggerPIN, HIGH);
+    if (subscription == &statusLight)
+    {
+      if (strcmp((char *)statusLight.lastread, "On") == 0)
+      {
+        #ifdef DEBUG
+          Serial.println("turning light on");
+        #endif
+        digitalWrite(relayTriggerPIN, HIGH);
+      }
+      if (strcmp((char *)statusLight.lastread, "Off") == 0) 
+      {
+        #ifdef DEBUG
+          Serial.println("turning light off");
+        #endif
+        digitalWrite(relayTriggerPIN, LOW); 
+      }
+    }
   }
-
-  #ifdef SLEEP
-    //  deep sleep the ESP8266 for specified time frame
-    #ifdef DEBUG
-      Serial.println("Going into deep sleep mode");
-    #endif
-    ESP.deepSleep(SLEEP_LENGTH);
-  #endif
 }
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() 
+{
+  // Stop if already connected.
+  if (mqtt.connected()) 
+  {
+    return;
+  }
+  #ifdef DEBUGF
+    Serial.println("Connecting to MQTT");
+  #endif
+
+  uint8_t retries = 3;
+  while (mqtt.connect() != 0)
+  //while ((ret = mqtt.connect()) != 0)
+  {
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000);  // wait 5 seconds
+    retries--;
+    if (retries == 0) 
+    {
+      // basically die and wait for WDT to reset me
+      while (1);
+    }
+  }
+  Serial.println("MQTT Connected!");
+}
+
+// Generic
+// boolean connectToBroker()
+// {
+//   // if the MQTT client is not connected:
+//   if (!mqttClient.connect(broker, port))
+//   {
+//     // print out the error message:
+//     #ifdef DEBUG
+//       Serial.print("MOTT connection failed. Error no: ");
+//       Serial.println(mqttClient.connectError());
+//     #endif
+//     // return that you're not connected:
+//     return false;
+//   }
+//   return true;
+// }
